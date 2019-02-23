f55941040ce4e6c22c7a7ec9c7f564bbcdd73132
Thomas <thomas@craftacademy.se>
2019-02-23 15:00:52 +0100

    adds navigation links to Header

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,10 +1,24 @@
 import React from 'react'
+import { NavLink, Link } from 'react-router-dom'
 
 const Header = () => {
   return (
-    <nav className="header">
-      <h1 className="header-title">My Portfolio</h1>
-    </nav>
+    <>
+      <nav className="header">
+        <h1 className="header-title">
+          <Link className="text-white hover:text-grey no-underline" to='/'>My Portfolio</Link>
+        </h1>
+        <ul className="list-reset flex">
+          <li className="mr-6">
+            <NavLink to='/about' className="text-white hover:text-grey no-underline" activeStyle={{ fontWeight: 'bold' }}>About Me</NavLink>
+          </li>
+          <li className="mr-6">
+            <NavLink to='/projects' className="text-white hover:text-grey no-underline" activeStyle={{ fontWeight: 'bold' }}>Projects</NavLink>
+          </li>
+        </ul>
+      </nav>
+
+    </>
   )
 }
 
