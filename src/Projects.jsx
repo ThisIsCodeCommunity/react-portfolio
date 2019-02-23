bee05c5b06161c1877fa710d5d83cb6d57ac12f5
Thomas <thomas@craftacademy.se>
2019-02-23 15:31:20 +0100

    reads project info from json file using axios

--- src/Projects.jsx
+++ src/Projects.jsx
@@ -1,12 +1,37 @@
-import React from 'react'
+import React, { Component } from 'react'
+import axios from 'axios'
 
-const Projects = () => {
-  return (
-    <div>
-      <h1 className="content-text">My Projects</h1>
-      <p className="content-text">Ipsum dolor dolorem consectetur est velit fugiat. Dolorem provident corporis fuga saepe distinctio ipsam? Et quos harum excepturi dolorum molestias?</p>
-    </div>
-  )
+class Projects extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      projects: []
+    }
+  }
+
+  componentDidMount() {
+    this.readProjects()
+  }
+
+  async readProjects() {
+    let projects = await axios.get('./data/projects.json')
+    this.setState({ projects: projects.data })
+  }
+
+  render() {
+    let projectList = this.state.projects.map(project => {
+      return (
+        <li key={project.id}>{project.name}</li>
+      )
+    })
+    return (
+      <div>
+        <h1 className="content-text">My Projects</h1>
+        <p className="content-text">Ipsum dolor dolorem consectetur est velit fugiat. Dolorem provident corporis fuga saepe distinctio ipsam? Et quos harum excepturi dolorum molestias?</p>
+        <ul>{projectList}</ul>
+      </div>
+    )
+  }
 }
 
 export default Projects
